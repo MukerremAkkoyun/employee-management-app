d883fa8909a23e6c60654fec913efd5dfacfeca6
Mükerrem Akkoyun <mukerrem.akkoyun@gmail.com>
2024-11-05 11:12:24 +0300

    chore: initialize project setup with Lit JS and basic folder structure

---
+++ .eleventy.cjs
@@ -0,0 +1,33 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addPassthroughCopy({
+    'src/i18n/**/*.json': 'src/i18n/',
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    'src/data/**/*.json': 'src/data/',
+  });
+
+  eleventyConfig.addPassthroughCopy({
+    'src/styles/global.css': 'src/styles/global.css',
+  });
+
+  eleventyConfig.addPassthroughCopy(
+    'node_modules/@webcomponents/webcomponentsjs'
+  );
+  eleventyConfig.addPassthroughCopy('node_modules/lit/polyfill-support.js');
+  return {
+    dir: {
+      input: 'src',
+      output: 'docs',
+    },
+    // pathPrefix: '/docs/',
+    templateExtensionAliases: {
+      '11ty.cjs': '11ty.js',
+      '11tydata.cjs': '11tydata.js',
+    },
+  };
+};
